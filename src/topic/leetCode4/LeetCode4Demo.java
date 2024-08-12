bc24125901f2e2be4aa73f14c398e19be67e40c1
jiangfan <961352198@qq.com>
2024-08-12 15:11:37 +0800

    feat:力扣4

---
+++ src/topic/leetCode4/LeetCode4Demo.java
@@ -0,0 +1,22 @@
+package topic.leetCode4;
+
+import util.IntMergeUitl;
+
+public class LeetCode4Demo {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 4, 6};
+        int[] nums2 = {2, 3, 7, 8};
+
+        int[] merged = IntMergeUitl.mergeIntArray(nums1, nums2);
+        int length = merged.length;
+
+        if (length % 2 == 1) {
+            System.out.println(merged[length / 2]);
+        }
+        else {
+            System.out.println((merged[length / 2 - 1] + merged[length / 2]) / 2.0);
+        }
+    }
+}
